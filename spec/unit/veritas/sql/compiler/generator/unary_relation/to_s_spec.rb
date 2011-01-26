8f005307c9d60d5ffc36c76f051da1f55bf0c6e8
Dan Kubb <dan.kubb@gmail.com>
2011-01-25 20:00:29 -0800

    Simplified UnaryRelation#to_s spec

--- spec/unit/veritas/sql/compiler/generator/unary_relation/to_s_spec.rb
+++ spec/unit/veritas/sql/compiler/generator/unary_relation/to_s_spec.rb
@@ -15,39 +15,11 @@ describe Generator::UnaryRelation, '#to_s' do
   context 'when no object visited' do
     it_should_behave_like 'an idempotent method'
 
+    it { should respond_to(:to_s) }
+
     it { should be_frozen }
 
-    it { should == '' }
-  end
-
-  context 'when a base relation is visited' do
-    before do
-      object.visit_veritas_base_relation(base_relation)
-    end
-
-    it_should_behave_like 'a generated SQL expression'
-
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users"' }
-  end
-
-  context 'when a projection is visited' do
-    before do
-      object.visit_veritas_algebra_projection(base_relation.project([ :id, :name ]))
-    end
-
-    it_should_behave_like 'a generated SQL expression'
-
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name" FROM "users"' }
-  end
-
-  context 'when a rename is visited' do
-    before do
-      object.visit_veritas_algebra_rename(base_relation.rename(:id => :user_id))
-    end
-
-    it_should_behave_like 'a generated SQL expression'
-
-    it { should == 'SELECT DISTINCT "users"."id" AS "user_id", "users"."name", "users"."age" FROM "users"' }
+    its(:to_s) { should == '' }
   end
 
   context 'when a restriction is visited' do
@@ -57,27 +29,7 @@ describe Generator::UnaryRelation, '#to_s' do
 
     it_should_behave_like 'a generated SQL expression'
 
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" WHERE "users"."id" = 1' }
-  end
-
-  context 'when an order is visited' do
-    before do
-      object.visit_veritas_relation_operation_order(base_relation.order)
-    end
-
-    it_should_behave_like 'a generated SQL expression'
-
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id", "users"."name", "users"."age"' }
-  end
-
-  context 'when a reverse is visited' do
-    before do
-      object.visit_veritas_relation_operation_order(base_relation.order.reverse)
-    end
-
-    it_should_behave_like 'a generated SQL expression'
-
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id" DESC, "users"."name" DESC, "users"."age" DESC' }
+    its(:to_s) { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" WHERE "users"."id" = 1' }
   end
 
   context 'when a limit is visited' do
@@ -87,7 +39,7 @@ describe Generator::UnaryRelation, '#to_s' do
 
     it_should_behave_like 'a generated SQL expression'
 
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id", "users"."name", "users"."age" LIMIT 1' }
+    its(:to_s) { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id", "users"."name", "users"."age" LIMIT 1' }
   end
 
   context 'when an offset is visited' do
@@ -97,6 +49,6 @@ describe Generator::UnaryRelation, '#to_s' do
 
     it_should_behave_like 'a generated SQL expression'
 
-    it { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id", "users"."name", "users"."age" OFFSET 1' }
+    its(:to_s) { should == 'SELECT DISTINCT "users"."id", "users"."name", "users"."age" FROM "users" ORDER BY "users"."id", "users"."name", "users"."age" OFFSET 1' }
   end
 end
