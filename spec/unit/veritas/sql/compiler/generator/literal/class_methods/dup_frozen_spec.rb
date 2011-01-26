0fce56fa5e0ae2256391d595eefd28a8d8518a64
Dan Kubb <dan.kubb@gmail.com>
2011-01-25 19:20:14 -0800

    Added spec for Literal.dup_frozen

---
+++ spec/unit/veritas/sql/compiler/generator/literal/class_methods/dup_frozen_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Generator::Literal, '.dup_frozen' do
+  subject { object.dup_frozen(object_arg) }
+
+  let(:object) { Generator::Literal }
+
+  context 'with a frozen object' do
+    let(:object_arg) { Date.today.freeze }
+
+    it { should_not equal(object_arg) }
+
+    it { should == object_arg }
+  end
+
+  context 'with a non-frozen object' do
+    let(:object_arg) { Date.today }
+
+    it { should equal(object_arg) }
+  end
+end
