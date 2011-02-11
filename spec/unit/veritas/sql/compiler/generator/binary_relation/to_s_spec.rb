83673f101b447a58e3706dc5bed650e2c1abf9c7
Dan Kubb <dan.kubb@gmail.com>
2011-02-10 23:25:08 -0800

    Added more specs for BinaryRelation#to_s

--- spec/unit/veritas/sql/compiler/generator/binary_relation/to_s_spec.rb
+++ spec/unit/veritas/sql/compiler/generator/binary_relation/to_s_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe Generator::BinaryRelation, '#to_s' do
   subject { object.to_s }
 
-  let(:object) { described_class.new }
+  let(:id)            { Attribute::Integer.new(:id)                      }
+  let(:name)          { Attribute::String.new(:name)                     }
+  let(:age)           { Attribute::Integer.new(:age, :required => false) }
+  let(:header)        { [ id, name, age ]                                }
+  let(:body)          { [ [ 1, 'Dan Kubb', 35 ] ].each                   }
+  let(:base_relation) { BaseRelation.new('users', header, body)          }
+  let(:object)        { described_class.new                              }
 
   context 'when no object visited' do
     it_should_behave_like 'an idempotent method'
@@ -13,5 +19,35 @@ describe Generator::BinaryRelation, '#to_s' do
     it { should be_frozen }
 
     its(:to_s) { should == '' }
+  end
+
+  context 'when a difference is visited' do
+    before do
+      object.visit(base_relation.difference(base_relation))
+    end
+
+    it_should_behave_like 'a generated SQL expression'
+
+    its(:to_s) { should eql('SELECT "id", "name", "age" FROM "users" EXCEPT SELECT "id", "name", "age" FROM "users"') }
+  end
+
+  context 'when an intersection is visited' do
+    before do
+      object.visit(base_relation.intersect(base_relation))
+    end
+
+    it_should_behave_like 'a generated SQL expression'
+
+    its(:to_s) { should eql('SELECT "id", "name", "age" FROM "users" INTERSECT SELECT "id", "name", "age" FROM "users"') }
+  end
+
+  context 'when a union is visited' do
+    before do
+      object.visit(base_relation.union(base_relation))
+    end
+
+    it_should_behave_like 'a generated SQL expression'
+
+    its(:to_s) { should eql('SELECT "id", "name", "age" FROM "users" UNION SELECT "id", "name", "age" FROM "users"') }
   end
 end
