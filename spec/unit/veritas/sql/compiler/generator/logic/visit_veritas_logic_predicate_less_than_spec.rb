bc895f96e621761ff4b9fa78f78ed6d7289d89dc
Bernard Lambeau <blambeau@gmail.com>
2011-03-03 12:09:31 +0100

    Supposed this one should be .lt

--- spec/unit/veritas/sql/compiler/generator/logic/visit_veritas_logic_predicate_less_than_spec.rb
+++ spec/unit/veritas/sql/compiler/generator/logic/visit_veritas_logic_predicate_less_than_spec.rb
@@ -4,7 +4,7 @@ describe Generator::Logic, '#visit_veritas_logic_predicate_less_than' do
   subject { object.visit_veritas_logic_predicate_less_than(less_than) }
 
   let(:described_class) { Class.new(Visitor) { include Generator::Logic } }
-  let(:less_than)       { Attribute::Integer.new(:id).gt(1)               }
+  let(:less_than)       { Attribute::Integer.new(:id).lt(1)               }
   let(:object)          { described_class.new                             }
 
   it_should_behave_like 'a generated SQL expression'
