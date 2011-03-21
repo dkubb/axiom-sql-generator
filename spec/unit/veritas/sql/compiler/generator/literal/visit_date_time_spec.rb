fca10625a6691304c58f881fdd6c70c81b909a05
Dan Kubb <dan.kubb@gmail.com>
2011-03-20 23:50:24 -0700

    DateTime with fractional seconds + 59 fails with rbx 1.2.3

--- spec/unit/veritas/sql/compiler/generator/literal/visit_date_time_spec.rb
+++ spec/unit/veritas/sql/compiler/generator/literal/visit_date_time_spec.rb
@@ -19,8 +19,8 @@ describe Generator::Literal, '#visit_date_time' do
       its(:to_s) { should eql("'2010-12-31T23:59:59.000000000+00:00'") }
     end
 
-    # rubinius 1.2.1 has problems with fractional seconds above 59
-    unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx' && Rubinius::VERSION <= '1.2.1'
+    # rubinius 1.2.3 has problems with fractional seconds above 59
+    unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx' && Rubinius::VERSION <= '1.2.3'
       context 'and the nanoseconds are greater than 0' do
         let(:nsec)      { 1                                                                       }
         let(:date_time) { DateTime.new(2010, 12, 31, 23, 59, 59 + nsec_in_seconds, offset).freeze }
@@ -44,8 +44,8 @@ describe Generator::Literal, '#visit_date_time' do
       its(:to_s) { should eql("'2010-12-31T23:59:59.000000000+00:00'") }
     end
 
-    # rubinius 1.2.1 has problems with fractional seconds above 59
-    unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx' && Rubinius::VERSION <= '1.2.1'
+    # rubinius 1.2.3 has problems with fractional seconds above 59
+    unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx' && Rubinius::VERSION <= '1.2.3'
       context 'and the nanoseconds are greater than 0' do
         let(:nsec)      { 1                                                                       }
         let(:date_time) { DateTime.new(2010, 12, 31, 15, 59, 59 + nsec_in_seconds, offset).freeze }
