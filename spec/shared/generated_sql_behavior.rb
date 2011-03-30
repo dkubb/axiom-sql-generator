3d288eb8d4653f3501a32bbfe3b00c32bfe978d8
Dan Kubb <dan.kubb@gmail.com>
2011-03-29 23:37:44 -0700

    Removed explicit spec for UTF-8 encoded SQL strings
    * The behaviour of each ruby implementation is so different that
      I don't want to gaurantee this in my library. I was able to get
      some consistency by using String#force_encoding everytime a String
      literal is created, but since that method is only available on 1.9
      it made for some nasty conditional code in about 20-25 methods.
    
      I think I could've made a helper method that would hide a lot of the
      complexity, but I didn't want to go down the road of specifying
      something which barely works across most rubies.

--- spec/shared/generated_sql_behavior.rb
+++ spec/shared/generated_sql_behavior.rb
@@ -4,12 +4,6 @@ shared_examples_for 'a generated SQL expression' do
   it { should respond_to(:to_s) }
 
   its(:to_s) { should be_kind_of(String) }
-
-  if String.public_method_defined?(:encoding)
-    it 'is UTF-8 encoded' do
-      subject.to_s.encoding.should equal(Encoding::UTF_8)
-    end
-  end
 end
 
 shared_examples_for 'a generated SQL SELECT query' do
