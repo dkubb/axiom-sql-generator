db178993edfe6785142f16296450093510f9d0c6
Dan Kubb <dan.kubb@gmail.com>
2011-05-26 14:11:13 -0700

    Skip specs that explode on rbx 1.2.3 due to a bug in it's implementation

--- spec/unit/date_time/iso_8601_spec.rb
+++ spec/unit/date_time/iso_8601_spec.rb
@@ -16,97 +16,100 @@ describe DateTime, '#iso8601' do
   let(:object)          { described_class.new(2010, 12, 31, 23, 59, 59 + nsec_in_seconds) }
   let(:nsec_in_seconds) { 1 - Rational(1, 10**9)                                          }
 
-  context 'with no arguments' do
-    subject { object.iso8601 }
+  # rubinius 1.2.3 has problems with fractional seconds above 59
+  unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx' && Rubinius::VERSION <= '1.2.3'
+    context 'with no arguments' do
+      subject { object.iso8601 }
 
-    context 'when the datetime is frozen' do
-      before do
-        object.freeze
+      context 'when the datetime is frozen' do
+        before do
+          object.freeze
+        end
+
+        it { should respond_to(:to_s) }
+
+        it_supports_nanoseconds do
+          should == '2010-12-31T23:59:59+00:00'
+        end
       end
 
-      it { should respond_to(:to_s) }
+      context 'when the datetime is not frozen' do
+        it { should respond_to(:to_s) }
 
-      it_supports_nanoseconds do
-        should == '2010-12-31T23:59:59+00:00'
-      end
-    end
-
-    context 'when the datetime is not frozen' do
-      it { should respond_to(:to_s) }
-
-      it { should == '2010-12-31T23:59:59+00:00' }
-    end
-  end
-
-  context 'with a time scale of 0' do
-    subject { object.iso8601(time_scale) }
-
-    let(:time_scale) { 0 }
-
-    context 'when the datetime is frozen' do
-      before do
-        object.freeze
-      end
-
-      it { should respond_to(:to_s) }
-
-      it_supports_nanoseconds do
-        should == '2010-12-31T23:59:59+00:00'
+        it { should == '2010-12-31T23:59:59+00:00' }
       end
     end
 
-    context 'when the datetime is not frozen' do
-      it { should respond_to(:to_s) }
+    context 'with a time scale of 0' do
+      subject { object.iso8601(time_scale) }
 
-      it { should == '2010-12-31T23:59:59+00:00' }
-    end
-  end
+      let(:time_scale) { 0 }
 
-  context 'with a time scale of 1' do
-    subject { object.iso8601(time_scale) }
+      context 'when the datetime is frozen' do
+        before do
+          object.freeze
+        end
 
-    let(:time_scale) { 1 }
+        it { should respond_to(:to_s) }
 
-    context 'when the datetime is frozen' do
-      before do
-        object.freeze
+        it_supports_nanoseconds do
+          should == '2010-12-31T23:59:59+00:00'
+        end
       end
 
-      it { should respond_to(:to_s) }
+      context 'when the datetime is not frozen' do
+        it { should respond_to(:to_s) }
 
-      it_supports_nanoseconds do
-        should == '2010-12-31T23:59:59.9+00:00'
-      end
-    end
-
-    context 'when the datetime is not frozen' do
-      it { should respond_to(:to_s) }
-
-      it { should == '2010-12-31T23:59:59.9+00:00' }
-    end
-  end
-
-  context 'with a time scale of 9' do
-    subject { object.iso8601(time_scale) }
-
-    let(:time_scale) { 9 }
-
-    context 'when the datetime is frozen' do
-      before do
-        object.freeze
-      end
-
-      it { should respond_to(:to_s) }
-
-      it_supports_nanoseconds do
-        should == '2010-12-31T23:59:59.999999999+00:00'
+        it { should == '2010-12-31T23:59:59+00:00' }
       end
     end
 
-    context 'when the datetime is not frozen' do
-      it { should respond_to(:to_s) }
+    context 'with a time scale of 1' do
+      subject { object.iso8601(time_scale) }
 
-      it { should == '2010-12-31T23:59:59.999999999+00:00' }
+      let(:time_scale) { 1 }
+
+      context 'when the datetime is frozen' do
+        before do
+          object.freeze
+        end
+
+        it { should respond_to(:to_s) }
+
+        it_supports_nanoseconds do
+          should == '2010-12-31T23:59:59.9+00:00'
+        end
+      end
+
+      context 'when the datetime is not frozen' do
+        it { should respond_to(:to_s) }
+
+        it { should == '2010-12-31T23:59:59.9+00:00' }
+      end
+    end
+
+    context 'with a time scale of 9' do
+      subject { object.iso8601(time_scale) }
+
+      let(:time_scale) { 9 }
+
+      context 'when the datetime is frozen' do
+        before do
+          object.freeze
+        end
+
+        it { should respond_to(:to_s) }
+
+        it_supports_nanoseconds do
+          should == '2010-12-31T23:59:59.999999999+00:00'
+        end
+      end
+
+      context 'when the datetime is not frozen' do
+        it { should respond_to(:to_s) }
+
+        it { should == '2010-12-31T23:59:59.999999999+00:00' }
+      end
     end
   end
 end
