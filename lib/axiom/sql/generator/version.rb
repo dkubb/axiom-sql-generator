69ee7694b9709e27df3221d66f3320a98d881bff
Dan Kubb <dan.kubb@gmail.com>
2013-04-05 00:03:23 -0700

    Bump version to 0.1.0

--- lib/axiom/sql/generator/version.rb
+++ lib/axiom/sql/generator/version.rb
@@ -3,7 +3,10 @@
 module Axiom
   module SQL
     module Generator
-      VERSION = '0.0.7'
+
+      # Gem version
+      VERSION = '0.1.0'.freeze
+
     end # module Generator
   end # module SQL
 end # module Axiom
